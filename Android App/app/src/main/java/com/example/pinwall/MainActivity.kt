47c347b55a5ec5658633507192e0b8eebf690947
Daeyoung <deanjung@hanyang.ac.kr>
2020-11-30 00:04:21 +0900

    Implementation of Kotlin extensions

--- Android App/app/src/main/java/com/example/pinwall/MainActivity.kt
+++ Android App/app/src/main/java/com/example/pinwall/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.pinwall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //hello this is a comment
+
+        pinList
     }
 }
