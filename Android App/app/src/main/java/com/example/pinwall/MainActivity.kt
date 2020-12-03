8ef28bbbbdc6d54c5296133963d50432cd9aa3d4
Ivo Maag <maagivo1@students.zhaw.ch>
2020-12-03 21:53:34 +0900

    database put firebase

--- Android App/app/src/main/java/com/example/pinwall/MainActivity.kt
+++ Android App/app/src/main/java/com/example/pinwall/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.pinwall
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +15,48 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val db = FirebaseFirestore.getInstance()
+        val pinList = initializePinList()
 
+        //addPost(db, "hallo", "I just want to say hallo")
+        //addPost(db, "ja", "I just want to say ja")
 
-        val pinList = arrayListOf(
+        rv_postList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rv_postList.setHasFixedSize(true)
+        rv_postList.adapter = ListAdapater(pinList)
+    }
+
+    private fun initializePinList(): ArrayList<Post> {
+        return arrayListOf(
             Post(R.drawable.men, "Daeyoung", "Hello World!"),
             Post(R.drawable.women, "Yang Kyung", "你好，世界!"),
             Post(R.drawable.men, "Ivo Maag", "Hallo Welt"),
             Post(R.drawable.men, "Eunwoo", "안녕 세계!")
-
         )
-
-
-        rv_postList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        rv_postList.setHasFixedSize(true)
-
-        rv_postList.adapter = ListAdapater(pinList)
     }
 
+    private fun addPost(db: FirebaseFirestore, title: String, text: String) {
+        val post: MutableMap<String, String> = HashMap()
+        post["title"] = title
+        post["text"] = text
 
+        // Add a new document with a generated ID
+        val TAG = "Message"
+        db.collection("posts")
+            .add(post)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e ->
+                Log.w(
+                    TAG,
+                    "Error adding document",
+                    e
+                )
+            }
+    }
 }
